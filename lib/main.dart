bd6598ccb5bc6b1f17e66508496e21dec72b0825
kazuki_kimura <kazukidddd836@gmail.com>
2024-01-23 23:32:16 +0900

    Refactor widget positioning in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,63 +37,71 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    xPosition = MediaQuery.sizeOf(context).height / 2;
-    yPosition = MediaQuery.sizeOf(context).width / 2;
+    // 画面全体の幅
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    // 画面全体の高さ
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    // ステータスバーの高さ
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    // アプリバーの高さ
+    final double appBarHeight = AppBar().preferredSize.height;
+
+    // Scaffoldのボディの高さ
+    final double bodyHeight = screenHeight - statusBarHeight - appBarHeight;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: LayoutBuilder(builder: (context, constraints) {
-        final width = constraints.maxWidth;
-        final height = constraints.maxHeight;
-        return StreamBuilder<AccelerometerEvent>(
-            stream: accelerometerEventStream(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                // x軸のデータで縦方向（上下）の移動を制御
-                yPosition = snapshot.data!.y * 10;
+      body: StreamBuilder<AccelerometerEvent>(
+          stream: accelerometerEventStream(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // x軸のデータで縦方向（上下）の移動を制御
+              yPosition = snapshot.data!.y * 10;
 
-                // y軸のデータで横方向（左右）の移動を制御
-                xPosition = snapshot.data!.x * 10;
-              }
-              return Stack(
-                alignment: Alignment.center,
-                children: [
-                  const SizedBox.expand(),
-                  Positioned.fill(
-                      child: Container(
-                    color: Colors.blueAccent,
-                  )),
-                  Container(
-                    width: 300,
+              // y軸のデータで横方向（左右）の移動を制御
+              xPosition = snapshot.data!.x * 10;
+            }
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                const SizedBox.expand(),
+                Positioned.fill(
+                    child: Container(
+                  color: Colors.blueAccent,
+                )),
+                Container(
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.fromBorderSide(
+                      BorderSide(
+                        color: Color(0xFF000000),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: xPosition + (screenWidth / 2), // 中心からのオフセットを加算
+                  top: yPosition + (bodyHeight / 2), // 中心からのオフセットを加算
+                  child: Container(
+                    width: 10,
+                    height: 10,
                     decoration: const BoxDecoration(
                       shape: BoxShape.circle,
-                      border: Border.fromBorderSide(
-                        BorderSide(
-                          color: Color(0xFF000000),
-                          width: 1,
-                        ),
-                      ),
+                      color: Colors.red,
                     ),
                   ),
-                  // このWidgetを動かしたい
-                  Positioned(
-                    right: xPosition + (width / 2), // 中心からのオフセットを加算
-                    top: yPosition + (height / 2), // 中心からのオフセットを加算
-                    child: Container(
-                      width: 10,
-                      height: 10,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            });
-      }),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
